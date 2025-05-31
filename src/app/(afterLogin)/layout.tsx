dbc02f5c1072b4788011aa1bb155dc2f23b94a83
kimywann <kimywan10@gmail.com>
2025-05-31 22:31:58 +0900

    feat: AfterLoginLayout 컴포넌트 생성

--- src/app/(afterLogin)/layout.tsx
+++ src/app/(afterLogin)/layout.tsx
@@ -1,5 +1,8 @@
-const AfterLayout = () => {
-  return <div>layout</div>;
+export default function AfterLoginLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div>
+    애프터 로그인 레이아웃
+    {children}
+    </div>
+  )
 };
-
-export default AfterLayout;
