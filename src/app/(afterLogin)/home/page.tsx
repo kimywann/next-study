04e20fb2fef532030b1a6d4afc1bb5e63f36f5ac
kimywann <kimywan10@gmail.com>
2025-06-24 22:46:40 +0900

    feat: 로그인 후 홈 화면 구현
    - 추천 및 팔로우 피드를 전환할 수 있는 탭 UI를 구현했습니다.
    - 사용자가 새로운 게시글을 작성할 수 있는 폼 컴포넌트를 추가했습니다.
    - 탭과 게시글 폼을 포함하는 홈 화면의 기본 레이아웃을 구성했습니다.

--- src/app/(afterLogin)/home/page.tsx
+++ src/app/(afterLogin)/home/page.tsx
@@ -1,3 +1,30 @@
+import style from "./home.module.css";
+
+import Tab from "./_component/Tab";
+import TabProvider from "./_component/TabProvider";
+
+import PostForm from "./_component/PostForm";
+import Post from "../_component/Post";
+
 export default function Home() {
-  return <main>홈페이지</main>;
+  return (
+    <main className={style.main}>
+      <TabProvider>
+        <Tab />
+        <PostForm />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+      </TabProvider>
+    </main>
+  );
 }
